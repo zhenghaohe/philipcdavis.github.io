4a9ddb0e144ae3572eca6c5c2a079eed7d277548
Philip Davis <Phillysoul11@gmail.com>
2014-05-28 20:39:00 -0400

    About page centered, removed unnecessary css styles

---
+++ _site/gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var pngcrush = require('imagemin-pngcrush');
+
+gulp.task('compress', function () {
+    return gulp.src('assets/img/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngcrush()]
+        }))
+        .pipe(gulp.dest('assets/dist'));
+});
